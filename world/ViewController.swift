91fec92bb5dcf32f37a7b03f03058a0d882e20db
Sebastian Jabłonowski <sebastianjablonowski@MacBook-Air-Sebastian.local>
2019-01-08 22:44:54 +0100

    Initial Commit

---
+++ world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  world
+//
+//  Created by Sebastian Jabłonowski on 08/01/2019.
+//  Copyright © 2019 Sebastian Jabłonowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

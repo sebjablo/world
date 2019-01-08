102fb145727e64b1922e801e74235fa5eb2f6985
Sebastian Jabłonowski <sebastianjablonowski@MacBook-Air-Sebastian.local>
2019-01-08 23:46:14 +0100

    Pierwsza zmiana

--- world/ViewController.swift
+++ world/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("hello World hello world hello121231231")
+        print("hello 1234")
     }
 
 

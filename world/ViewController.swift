a7d50a91d59e94bf8f0560cb62eb6761be74bdaa
Sebastian Jabłonowski <sebastian.jablonowski@gmail.com>
2019-01-09 22:02:24 +0100

    nowa zmiana testowa

--- world/ViewController.swift
+++ world/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Uczę się obsługi systemu wrsionowania GIT")
+        print("Uczę się obsługi systemu wrsionowania GIT 12345")
     }
 
 
